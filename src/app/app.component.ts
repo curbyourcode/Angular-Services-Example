3aee1bd26be8ca99bc07460a23a32443f27839fd
curbyourcode <majlessirouz@gmail.com>
2019-01-21 14:57:51 -0800

    add ngOninit lifesycle hook to console log cars from service then bind it to someproperty to myData method from service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DataService } from './data.service';
 
 @Component({
@@ -6,10 +6,17 @@ import { DataService } from './data.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'service-example';
+  someProperty  = '';
 
   constructor (private dataService: DataService) {
 
   }
+
+  ngOnInit() {
+    console.log(this.dataService.cars);
+    this.someProperty = this.dataService.myData();
+  }
+
 }
